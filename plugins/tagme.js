08022416a5a590c5fdc311fddbe75ed2f9a306ab
Aadhi <luckyser439@gmail.com>
2025-02-25 01:05:02 +0530

    Add files via upload

---
+++ plugins/tagme.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text }) => {
+  let tag = `@${m.sender.replace(/@.+/, '')}`
+  let mentionedJid = [m.sender]
+  conn.reply(m.chat, tag, m, { contextInfo: { mentionedJid }})
+}
+handler.help = ['tagme']
+handler.tags = ['group']
+handler.command = /^tagme$/i
+export default handler
