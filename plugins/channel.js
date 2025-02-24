d64e0ef9ee8a06f5bc568924830846b0eeb284b8
Aadhi <luckyser439@gmail.com>
2025-02-25 01:29:13 +0530

    𝐅𝐢𝐱 𝐛𝐲 𝐝𝐚𝐫𝐤 𝐩𝐫𝐢𝐧𝐜𝐞

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 
 ╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+│ 📂 *BOT Name:* _XLICON-V2
+│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By Dark prince (Aadhi ser)._
+│ 👤 *Owner:* _Dark prince_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VayY2vu1NCrLQHzmRB2Q
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
